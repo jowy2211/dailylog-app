2e9ea5c01d72831cfa8fc0cb3e2d407cbe789571
unjust <jowy2211@gmail.com>
2024-06-22 21:58:57 +0700

    Remove Existing SourceCode

--- middleware/isAuth.js
+++
@@ -1,8 +0,0 @@
-export default function ({ app, redirect }) {
-  // If the user is not authenticated
-  if (!app.$auth.loggedIn || !app.$cookies.get('auth._user')) {
-    return redirect('/login');
-  }
-
-  return true;
-}
